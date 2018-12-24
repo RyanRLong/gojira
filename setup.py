6b7de6c63f05f53ae60c5df5ee911e8f4daff122
Ryan Long <ryan@saltycatfish.com>
2018-12-24 11:59:51 -0500

    Start setting up for distribution

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Gojira',
+      version='0.1',
+      description='Command line interface and macros for Jira',
+      url='http://github.com/storborg/funniest',
+      author='Ryan Long',
+      author_email='ryan@saltycatfish.com',
+      license='MIT',
+      packages=['gojira'],
+      zip_safe=False)
